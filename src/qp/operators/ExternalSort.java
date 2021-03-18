581502934d7ec783274235cb1a1a0242aeab507e
leon_sha <shalong5794@gmail.com>
2021-03-19 01:31:33 +0800

    add SortedRun & ExternalSort

---
+++ src/qp/operators/ExternalSort.java
@@ -0,0 +1,126 @@
+package qp.operators;
+
+import qp.utils.Batch;
+import qp.utils.Tuple;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+class ExternalSort extends Operator {
+    Operator base;
+    int compareIndex;
+    String outfile;
+    int filenum;
+    int numBuff;
+    int batchsize;
+    ArrayList<Batch> mem;
+    ArrayList<SortedRun> srs;
+    SortedRun finalSR;
+
+    public ExternalSort(int type, Operator base, int compareIndex, int numBuff) {
+        super(type);
+        this.base = base;
+        this.compareIndex = compareIndex;
+        this.schema = base.getSchema();
+        this.numBuff = numBuff;
+        mem = new ArrayList<>(numBuff);
+        filenum = 0;
+    }
+
+    @Override
+    public boolean open() {
+        int tuplesize = schema.getTupleSize();
+        batchsize = Batch.getPageSize() / tuplesize;
+
+        if(!base.open()) return false;
+        do {
+            ArrayList<Tuple> tuplesInMem = new ArrayList<>();
+            for (int i = 0; i < numBuff; ++i) {
+                Batch curr = base.next();
+                if (curr != null) mem.set(i, curr);
+                else break;
+                for (int j = 0; j < curr.size(); ++j)
+                    tuplesInMem.add(curr.get(j));
+            }
+            Collections.sort(tuplesInMem, this::compareTuplesByIndex);
+            srs.add(new SortedRun(tuplesInMem, batchsize));
+        } while (base.next() != null);
+
+        for (int pass = 0; ; pass++) {
+            if (srs.size() == 1) {
+                finalSR = srs.get(0);
+                return true;
+            }
+            srs = mergeSortedRuns(srs, mem);
+        }
+    }
+
+    @Override
+    public Batch next() {
+        return finalSR.next();
+    }
+
+    @Override
+    public boolean close() {
+        return super.close();
+    }
+
+    private int compareTuplesByIndex(Tuple left , Tuple right) {
+        return Tuple.compareTuples(left, right, compareIndex);
+    }
+    private int findMaxTupleIndex(ArrayList<Batch> mem, int[] cursors, boolean[] isNull) {
+        int maxIdx = -1; boolean foundNonNull = false;
+        for (int i = 0; i < mem.size(); ++i) {
+            if (isNull[i]) continue;
+            if (!foundNonNull) {
+                maxIdx = i;
+                foundNonNull = true;
+            }
+            if (compareTuplesByIndex(mem.get(i).get(cursors[i]), mem.get(maxIdx).get(cursors[maxIdx])) < 0) {
+                maxIdx = i;
+            }
+        }
+        return maxIdx;
+    }
+
+    private ArrayList<SortedRun> mergeSortedRuns(ArrayList<SortedRun> srs, ArrayList<Batch> mem) {
+        ArrayList<SortedRun> newSrs = new ArrayList<>(srs.size() % (numBuff-1) + 1);
+        for (int i = 0; i < srs.size(); i += (numBuff-1)) {
+            SortedRun outSr = newSrs.get(i/(numBuff-1));
+            outSr = new SortedRun(batchsize);
+            mem = new ArrayList<Batch>();
+            for (int j = 0; j < numBuff-1; ++j) {
+                Batch temp = srs.get(i+j).next();
+                if (temp != null)
+                    mem.add(temp);
+                else
+                    break;
+            }
+            int[] cursors = new int[mem.size()];
+            Arrays.fill(cursors, 0);
+            boolean[] isNull = new boolean[mem.size()];
+            Arrays.fill(isNull, false);
+            mem.add(new Batch(batchsize));
+            Batch outbatch = mem.get(mem.size()-1);
+            while (true) {
+                int maxIdx = findMaxTupleIndex(mem, cursors, isNull);
+                if (maxIdx == -1) {
+                    // all runs are used
+                    outSr.add(outbatch);
+                    break;
+                }
+                outbatch.add(mem.get(maxIdx).get(cursors[maxIdx]));
+                if (outbatch.isFull())
+                    outSr.add(outbatch);
+                cursors[maxIdx]++;
+                if (cursors[maxIdx] == mem.get(maxIdx).size()) {
+                    Batch temp = srs.get(i+maxIdx).next();
+                    if (temp != null) mem.set(maxIdx, temp);
+                    else isNull[maxIdx] = true;
+                }
+            }
+        }
+        return newSrs;
+    }
+}
---
+++ src/qp/operators/SortedRun.java
@@ -0,0 +1,116 @@
+package qp.operators;
+
+import qp.utils.Batch;
+import qp.utils.Tuple;
+
+import java.io.*;
+import java.util.ArrayList;
+
+class SortedRun {
+    static int fn = 0;
+    String filename;
+    int batchsize;
+    boolean eos;
+    ObjectInputStream in;
+    ObjectOutputStream out;
+
+    public SortedRun(int batchsize) {
+        fn++;
+        filename = "SortedRun-" + fn;
+        this.batchsize = batchsize;
+        try {
+            in = new ObjectInputStream(new FileInputStream(filename));
+        } catch (IOException e) {
+            System.err.println("SortedRun: Error reading " + filename);
+        }
+        try {
+            out = new ObjectOutputStream(new FileOutputStream((filename)));
+        } catch (IOException e) {
+            System.err.println("SortedRun: Error writing " + filename);
+        }
+    }
+
+    public SortedRun(ArrayList<Tuple> tuples, int batchsize) {
+        fn++;
+        filename = "SortedRun-" + fn;
+        this.batchsize = batchsize;
+        try {
+            in = new ObjectInputStream(new FileInputStream(filename));
+        } catch (IOException e) {
+            System.err.println("SortedRun: Error reading " + filename);
+        }
+        try {
+            out = new ObjectOutputStream(new FileOutputStream((filename)));
+        } catch (IOException e) {
+            System.err.println("SortedRun: Error writing " + filename);
+        }
+        Batch batch = new Batch(batchsize);
+        for (Tuple tuple : tuples) {
+            batch.add(tuple);
+            if (batch.isFull()) {
+                try {
+                    out.writeObject(batch);
+                } catch (IOException e) {
+                    System.err.println("SortedRun: cannot write object");
+                }
+                batch = new Batch(batchsize);
+            }
+        }
+        if (batch.size() > 0) {
+            try {
+                out.writeObject(batch);
+            } catch (IOException e) {
+                System.err.println("SortedRun: cannot write object");
+            }
+        }
+    }
+
+    public boolean add(Batch batch) {
+        try {
+            out.writeObject(batch);
+        } catch (IOException e) {
+            System.err.println("SortedRun: cannot write object");
+            return false;
+        }
+        return true;
+    }
+
+    public Batch next() {
+        /** The file reached its end and no more to read **/
+        if (eos) {
+            return null;
+        }
+        Batch tuples = new Batch(batchsize);
+        while (!tuples.isFull()) {
+            try {
+                Tuple data = (Tuple) in.readObject();
+                tuples.add(data);
+            } catch (ClassNotFoundException cnf) {
+                System.err.println("Scan:Class not found for reading file  " + filename);
+                System.exit(1);
+            } catch (EOFException EOF) {
+                /** At this point incomplete page is sent and at next call it considered
+                 ** as end of file
+                 **/
+                eos = true;
+                return tuples;
+            } catch (IOException e) {
+                System.err.println("Scan:Error reading " + filename);
+                System.exit(1);
+            }
+        }
+        return tuples;
+    }
+
+    public boolean close() {
+        try {
+            in.close();
+            out.close();
+        } catch (IOException io) {
+            System.err.println("SortedRun: Error closing file");
+            return false;
+        }
+        File file = new File(filename);
+        return true;
+    }
+}
